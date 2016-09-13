56ef55f0058354ddbc66777d99070a3016bb6e2b
Drew Poulin <drewpcchannel@gmail.com>
2016-09-12 17:59:37 -1000

    added in random div locations2

--- public/divTracker/divTrackerApp.js
+++ public/divTracker/divTrackerApp.js
@@ -1,9 +1,7 @@
 function populateDivs () {
   for(var x = 0; x < 20; x++) {
     var leftPos = Math.random() * 400 + "px";
-    // leftPos = Math.round(leftPos) + "px"
     var topPos = Math.random() * 400 + "px";
-    // topPos = Math.round(topPos) + "px"
     var newDiv = document.createElement("div");
     $(newDiv).height(30).width(30).css({
       position: "absolute",
