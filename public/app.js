ec413e7fcfca01a7429131f25e7ce86f27e38192
Drew Poulin <drewpcchannel@gmail.com>
2016-09-12 16:07:39 -1000

    created first image test

---
+++ public/app.js
@@ -0,0 +1,4 @@
+// $ for jQuery (like const jQuery = require('jquery'))
+// targetsA is the item ID
+// hide method, returns the DOM div and show fades it on 4 seconds later
+$(".targetsA").hide().show(4000);
