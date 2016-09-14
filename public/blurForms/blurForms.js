bfd8371e5ad9976f7ec15e564b27664c79406776
Drew Poulin <drewpcchannel@gmail.com>
2016-09-13 16:04:23 -1000

    added in forms that validate in real time

--- public/blurForms/blurForms.js
+++ public/blurForms/blurForms.js
@@ -1,3 +1,16 @@
-$('input').blur(function () {
+$('.formsContainer').blur(function () {
   console.log('blur triggered')
+})
+
+$('.validFormIn').keydown(function(event) {
+  if (!isNaN(event.key)) {
+    $('.validFormIn').css({
+      background: "#e3bfce"
+    })
+    return false;
+  } else {
+    $('.validFormIn').css({
+      background: "#ffffff"
+    })
+  }
 })
